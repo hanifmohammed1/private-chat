d83d17d4c51b70c19d8e7981d81c055bc546d7f7
Hanif Mohammed <hanif.mohammed1@outlook.com>
2025-05-22 14:48:01 +0100

    Organize Firebase configuration and fix initialization

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAzWD-GapOQ5XYvqFZUjoJsWB1yzHxrd1I",
+    authDomain: "privatechat-317bc.firebaseapp.com",
+    databaseURL: "https://privatechat-317bc-default-rtdb.firebaseio.com",
+    projectId: "privatechat-317bc",
+    storageBucket: "privatechat-317bc.firebasestorage.app",
+    messagingSenderId: "742830327113",
+    appId: "1:742830327113:web:84662100302df40dca4698",
+    measurementId: "G-NX49XRRQCZ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
